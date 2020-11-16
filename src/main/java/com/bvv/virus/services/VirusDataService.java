0e57ba13f7e6558806429b014562f445db6b9f3f
Vadim Bond <wadimbond@mail.ru>
2020-11-16 02:30:31 +0100

    added VirusDataService

--- src/main/java/com/bvv/virus/services/VirusDataService.java
+++ src/main/java/com/bvv/virus/services/VirusDataService.java
@@ -1,4 +1,39 @@
 package com.bvv.virus.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
 public class VirusDataService {
+
+  private final static String VIRUS_DATASOURCE_URL = "https://raw.githubusercontent" +
+    ".com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv";
+
+  //  fetch the data
+  @PostConstruct
+  public void fetchVirusData() throws IOException, InterruptedException {
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder()
+      .uri(URI.create(VIRUS_DATASOURCE_URL))
+      .build();
+    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+    StringReader csvReader = new StringReader(response.body());
+    Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvReader);
+
+    for (CSVRecord record: records) {
+      String provinceState = record.get("Province/State");
+      System.out.println(provinceState);
+    }
+  }
+
 }
