2563349468de42f790b8102ea7e90cb808796e29
Vadim Bond <wadimbond@mail.ru>
2020-11-17 00:25:12 +0100

    added controllers

--- src/main/java/com/bvv/virus/services/VirusDataService.java
+++ src/main/java/com/bvv/virus/services/VirusDataService.java
@@ -1,7 +1,9 @@
 package com.bvv.virus.services;
 
+import com.bvv.virus.models.Location;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -11,6 +13,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class VirusDataService {
@@ -18,9 +22,15 @@ public class VirusDataService {
   private final static String VIRUS_DATASOURCE_URL = "https://raw.githubusercontent" +
     ".com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv";
 
+  private List<Location> allStats = new ArrayList<>();
+
   //  fetch the data
   @PostConstruct
+  //  run on a schedule cron (every twelve hours)
+  @Scheduled(cron = "0 0 */12 ? * *")
   public void fetchVirusData() throws IOException, InterruptedException {
+    List<Location> newStats = new ArrayList<>();
+
     HttpClient client = HttpClient.newHttpClient();
     HttpRequest request = HttpRequest.newBuilder()
       .uri(URI.create(VIRUS_DATASOURCE_URL))
@@ -31,9 +41,15 @@ public class VirusDataService {
     Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvReader);
 
     for (CSVRecord record: records) {
-      String provinceState = record.get("Province/State");
-      System.out.println(provinceState);
+      Location location = new Location();
+      location.setState(record.get("Province/State"));
+      location.setCountry(record.get("Country/Region"));
+      location.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
+      System.out.println(location);
+      newStats.add(location);
     }
+
+    this.allStats = newStats;
   }
 
 }
